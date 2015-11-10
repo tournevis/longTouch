4284addfc78f22c4ad606e1ec8942289a9b7d269
ROBERT Arthur <arobert@p-212b-2.local>
2015-11-10 14:28:45 +0100

    Initial Commit

---
+++ longtouch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  longtouch
+//
+//  Created by ROBERT Arthur on 10/11/2015.
+//  Copyright © 2015 ROBERT Arthur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
